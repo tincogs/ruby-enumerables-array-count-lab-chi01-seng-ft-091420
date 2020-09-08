13b989a62fd3ea32b4116a1fe9605f8e4cfccc41
Tom ODay <tomoday91@gmail.com>
2020-09-08 16:43:02 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,11 +1,11 @@
 def count_strings(array)
   array.count do |element|
-    element.is_a? String
+    element == String
   end
 end
 
 def count_empty_strings(array)
   array.count do |element|
-    element == " "
+    element == ""
   end
 end
